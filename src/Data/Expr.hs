6d1f224b6e24c6678d4224916fe0d263e36bc4f6
David Obwaller <david.obwaller@uibk.ac.at>
2018-11-11 18:14:04 +0100

    Added `Data.Expr.Pretty` module

--- src/Data/Expr.hs
+++ src/Data/Expr.hs
@@ -3,4 +3,5 @@ module Data.Expr (
   ) where
 
 import           Data.Expr.Parser as E
+import           Data.Expr.Pretty as E
 import           Data.Expr.Types  as E
--- src/Data/Expr/Types.hs
+++ src/Data/Expr/Types.hs
@@ -20,28 +20,3 @@ data Expr where
   Fun :: Text -> [Text] -> Expr
   -- match x with | nil -> e | <x, x, x> -> e
   Match :: Text -> Expr -> (Text, Text, Text, Expr) -> Expr
-
-class Pretty t where
-  pretty :: t -> Text
-
-instance Pretty Text where
-  pretty = id
-
-instance Pretty Expr where
-  pretty (T x y z) = "<" <> T.intercalate ", " (map pretty [x, y, z]) <> ">"
-  pretty (V x) = pretty x
-
-  pretty (B True) = "true"
-  pretty (B False) = "false"
-
-  pretty (Ite p e1 e2) = "if " <> pretty p <> " then " <> pretty e1 <> " else " <> pretty e2
-
-  pretty (Fun f as) = pretty f <> " " <> T.intercalate " " (map pretty as)
-
-  pretty (Let x e1 e2) = "let " <> pretty x <> " = " <> pretty e1 <> " in " <> pretty e2
-
-  pretty (Match e e1 (x1, x2, x3, e2)) = "match " <> pretty e <> " with | nil -> " <> pretty e1 <> " | <" <> T.intercalate ", " (map pretty [x1, x2, x3]) <> "> -> " <> pretty e2
-
-  pretty (l :<  r) = pretty l <> " < " <> pretty r
-  pretty (l :== r) = pretty l <> " == " <> pretty r
-  pretty (l :>  r) = pretty l <> " > " <> pretty r
