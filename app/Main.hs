5f8f37086bd117a43bbdb2fdd2ec83d26f1ff23e
David Obwaller <david.obwaller@uibk.ac.at>
2018-11-13 16:54:08 +0100

    Added `--ast` flag to executable

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,19 @@ module Main where
 
 import           Data.Expr
 
-import           Control.Monad      (forM_)
-import           Data.Text.IO       as T
-import           System.Environment
-import           Text.Parsec        (parse, many1)
+import           Control.Monad          (forM_)
+import           Data.Text.IO           as T
+import           System.Environment.Ext
+import           Text.Parsec            (many1, parse)
 
 main :: IO ()
 main = do
-  args <- getArgs
+  (flags, args) <- partitionArgs <$> getArgs
   forM_ args $ \arg -> do
     r <- parse (many1 decl) arg <$> T.readFile arg
     case r of
       Left e -> print e
-      Right decls -> mapM_ (T.putStrLn . pretty) decls
+      Right decls -> mapM_ f decls
+        where
+          f | "--ast" `elem` flags = print
+            | otherwise            = T.putStrLn . pretty
