987d6aa4e5a3bbafe5e6ee73e557b576ae6d0760
David Obwaller <david.obwaller@uibk.ac.at>
2018-11-14 11:50:37 +0100

    Updated evaluator

--- src/Lac/Eval.hs
+++ src/Lac/Eval.hs
@@ -1,21 +1,18 @@
 module Lac.Eval where
 
-import           Data.Expr
+import           Data.Expr.Types
 
 import qualified Data.List.NonEmpty as NE
 import           Data.Map           (Map)
 import qualified Data.Map           as M
 import           Data.Text          (Text)
 
-data Tree a
-  = TNode (Tree a) a (Tree a)
-  | TNil
-  deriving (Eq, Show)
-
 eval :: Map Text Literal -> Expr -> Literal
 eval env expr =
   case expr of
+    L (LNode e1 e2 e3) -> LNode (L $ eval env e1) (L $ eval env e2) (L $ eval env e3)
     L l -> l
+
     V x -> let Just l = M.lookup x env in l
 
     e1 :<  e2 -> undefined
@@ -28,7 +25,7 @@ eval env expr =
         LBool False -> eval env e3
         _ -> error "e1 evaluates to non-Boolean value"
 
-    Let _ _ _ -> undefined
+    Let x e1 e2 -> eval (M.insert x (eval env e1) env) e2
 
     Fun _ _ -> undefined
 
@@ -36,8 +33,12 @@ eval env expr =
   where
     match :: Literal -> [(Pattern, Expr)] -> Literal
     match _ [] = undefined -- TODO: run-time exception
-    match LNil ((PNil, e) : xs) = eval env e
-    match (LNode e1 e2 e3) ((PNode x1 x2 x3, e) : xs) =
-      -- TODO: bind x1, x2, x3 to environment
-      undefined
+    match LNil ((PNil, e) : _) = eval env e
+    match (LNode e1 e2 e3) ((PNode x1 x2 x3, e) : _) =
+      let env' = M.insert x1 (eval env e1)
+               . M.insert x2 (eval env e2)
+               . M.insert x3 (eval env e3)
+               $ env
+      in
+      eval env' e
     match l (_ : xs) = match l xs
