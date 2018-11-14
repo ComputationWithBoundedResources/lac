bfc167a3486ee00556142ac7e0c27f59c4072a61
David Obwaller <david.obwaller@uibk.ac.at>
2018-11-14 12:54:06 +0100

    Evaluate expressions when `--eval` flag is given

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,9 @@
 module Main where
 
 import           Data.Expr
+import           Lac.Eval
 
-import           Control.Monad          (forM_)
+import           Control.Monad          (forM_, when)
 import           Data.Text.IO           as T
 import           System.Environment.Ext
 import           Text.Parsec            (many1, parse)
@@ -14,7 +15,12 @@ main = do
     r <- parse (many1 decl) arg <$> T.readFile arg
     case r of
       Left e -> print e
-      Right decls -> mapM_ f decls
+      Right decls ->
+        do
+          mapM_ f decls
+          when ("--eval" `elem` flags) $
+            forM_ decls $ \(Decl _ _ e) ->
+                print $ eval mempty e
         where
           f | "--ast" `elem` flags = print
             | otherwise            = T.putStrLn . pretty
