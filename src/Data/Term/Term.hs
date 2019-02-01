9dae0ee1a8cf0c315b67bd2dbeccb99c3ce265e3
David Obwaller <david.obwaller@uibk.ac.at>
2019-02-01 17:07:28 +0100

    Made `Data.Term.Term.T` a `Bifunctor` instance

--- src/Data/Term/Term.hs
+++ src/Data/Term/Term.hs
@@ -1,14 +1,18 @@
 module Data.Term.Term where
 
+import Data.Bifunctor
+
 data T f v
   = V v
   | F f [T f v]
   deriving (Eq, Show)
 
+instance Bifunctor T where
+  bimap _ g (V x)    = V $ g x
+  bimap f g (F x ts) = F (f x) (map (bimap f g) ts)
+
 mapVar :: (v -> w) -> T f v -> T f w
-mapVar g (V x)    = V (g x)
-mapVar g (F f ts) = F f (map (mapVar g) ts)
+mapVar = second
 
 mapFun :: (f -> g) -> T f v -> T g v
-mapFun f (V x)    = V x
-mapFun f (F x xs) = F (f x) (map (mapFun f) xs)
+mapFun = first
