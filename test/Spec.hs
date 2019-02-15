dd743156beb630741ca593325913aefe832a163d
David Obwaller <david.obwaller@uibk.ac.at>
2019-02-15 16:09:41 +0100

    Added test cases for `letNF`

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import           Data.Expr.Types
+
+import           Control.Monad   (void)
+import           System.Exit
+import           Test.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+  do
+    Counts _ _ e f <- runTestTT tests
+    if e + f > 0
+      then exitFailure
+      else exitSuccess
+  where
+    tests = TestList [ TestLabel "testLetNF1" testLetNF1
+                     , TestLabel "testLetNF2" testLetNF2
+                     ]
+
+iteExpr p = Ite p (Var "e1") (Var "e2")
+  where
+
+testLetNF1 =
+  let expr = iteExpr (Lit (LBool True))
+  in
+  TestCase $ assertEqual "let-NF #1" expr (letNF expr)
+
+testLetNF2 =
+  let cmp = Cmp CmpEq (Var"x") (Var"y")
+      expr = iteExpr cmp
+  in
+  TestCase $ assertBool "let-NF #2" $ expr /= letNF expr
