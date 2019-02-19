a2986df7b308ba20c5c467ac3a06f5508a0cfe61
David Obwaller <david.obwaller@uibk.ac.at>
2019-02-19 17:00:57 +0100

    Added executable `lnf` for converting source to let normal form

---
+++ exe/lnf/Main.hs
@@ -0,0 +1,34 @@
+module Main where
+
+import           Data.Expr
+
+import qualified Data.Text.IO       as T
+import           System.Environment
+import           System.Exit
+import           Text.Parsec        (parse)
+import Debug.Trace
+
+main :: IO ()
+main =
+  do
+    args <- getArgs
+    mapM_ process args
+
+process :: FilePath -> IO ()
+process path =
+  do
+    r <- parse prog path <$> T.readFile path
+    case r of
+      Left _ -> exitFailure
+      Right decls -> do
+        --traceShowM decls
+        --mapM_ (T.putStrLn . ppDecl) decls
+        mapM_ (T.putStrLn . ppDecl . letNF') decls
+
+  where
+    ppDecl (Decl x xs e) = pretty e
+
+    letNF' (Decl x xs e) =
+      let e' = (toLetNF e)
+      in
+      Decl x xs e'
