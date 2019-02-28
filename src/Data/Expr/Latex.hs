d77a10565e26963301033f520034d7af959f5779
David Obwaller <david.obwaller@uibk.ac.at>
2019-02-28 17:24:29 +0100

    Added LaTeX output for literals

--- src/Data/Expr/Latex.hs
+++ src/Data/Expr/Latex.hs
@@ -18,8 +18,18 @@ instance Latex Expr where
         "\\mathrm{if}\\;" <> latex x
           <> "\\; \\mathrm{then}\\;" <> latex e1
           <> "\\; \\mathrm{else}\\;" <> latex e2
+      Lit l -> latex l
       _ -> "\\mathrm{TODO}"
 
 instance Latex Pattern where
   latex PNil          = "\\mathrm{nil}"
   latex (PNode a b c) = "\\langle " <> T.intercalate ", " [a, b, c] <> " \\rangle"
+
+instance Latex Literal where
+  latex LNil = "\\mathsf{nil}"
+  latex (LNode e1 e2 e3) = "\\langle " <> T.intercalate ", " (map latex [e1, e2, e3]) <> " \\rangle"
+
+  latex (LBool True) = "\\mathtt{true}"
+  latex (LBool False) = "\\mathtt{true}"
+
+  latex (LNat n) = T.pack (show n)
