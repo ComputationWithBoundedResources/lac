666dc5279bd735183ac978ca57a2d18c4f1a35d0
David Obwaller <david.obwaller@uibk.ac.at>
2019-06-02 16:42:50 +0200

    Added example code for tree traversals
    - adapted from Lorenz' examples

---
+++ examples/traversals.ml
@@ -0,0 +1,14 @@
+inorder t q =
+  match t with
+    | nil -> q
+    | {l, x, r} -> {nil, x, inorder l (inorder r q)};
+
+preorder t q =
+  match t with
+    | nil -> q
+    | {l, x, r} -> {nil, x, preorder l (preorder r q)};
+
+postorder t q =
+  match t with
+    | nil -> q
+    | {l, x, r} -> postorder l (postorder r {nil, x, q});
