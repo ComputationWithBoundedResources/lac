46697c9643a376b4e13a8262a282e6291b0d3f88
David Obwaller <david.obwaller@uibk.ac.at>
2019-06-11 17:36:38 +0200

    Added base module for inference rules

---
+++ src/Lac/Analysis/Rules/Common.hs
@@ -0,0 +1,7 @@
+module Lac.Analysis.Rules.Common (
+    module E
+  ) where
+
+import           Data.Expr.Typed    as E
+import           Data.Text          as E (Text)
+import           Lac.Analysis.Types as E
