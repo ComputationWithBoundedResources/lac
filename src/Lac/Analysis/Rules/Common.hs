c0596d179d6350d511b845c3ad65c8e9a5ed32b8
David Obwaller <david.obwaller@uibk.ac.at>
2019-06-13 21:01:05 +0200

    Export `Data.Bound` and `Data.Type` module from `Lac.Analysis.Rules.Common`

--- src/Lac/Analysis/Rules/Common.hs
+++ src/Lac/Analysis/Rules/Common.hs
@@ -2,6 +2,8 @@ module Lac.Analysis.Rules.Common (
     module E
   ) where
 
+import           Data.Bound         as E
 import           Data.Expr.Typed    as E
 import           Data.Text          as E (Text)
+import           Data.Type          as E
 import           Lac.Analysis.Types as E
