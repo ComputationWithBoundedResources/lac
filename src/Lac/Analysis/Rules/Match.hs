ff60e08ca6040c9f19c173cba2cff534b0e9533c
David Obwaller <david.obwaller@uibk.ac.at>
2019-06-13 21:01:22 +0200

    Started to implement (match) rule

--- src/Lac/Analysis/Rules/Match.hs
+++ src/Lac/Analysis/Rules/Match.hs
@@ -2,5 +2,22 @@ module Lac.Analysis.Rules.Match where
 
 import           Lac.Analysis.Rules.Common
 
-ruleMatch :: Ctx -> Text -> Typed -> Typed -> Gen ()
-ruleMatch = undefined
+import qualified Data.Text.IO              as T
+
+ruleMatch :: Ctx -> Text -> Typed -> (Text, Text, Text) -> Typed -> Gen Ctx
+ruleMatch ctx x e1 (x1, x2, x3) e2 =
+  do
+    let u = Bound 1
+
+    (_, p) <- splitCtx u ctx [x]
+    liftIO $ do
+      putStrLn "P"
+      T.putStrLn (ppCtx p)
+
+    (_, r) <- splitCtx u ctx [x]
+    r' <- augmentCtx u r [(x1, tyTree), (x2, tyNat), (x3, tyTree)]
+    liftIO $ do
+      putStrLn "R"
+      T.putStrLn (ppCtx r')
+
+    return ctx
