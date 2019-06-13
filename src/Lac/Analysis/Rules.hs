78cd0b712c94b059dd1d72593c76e0a2e3bcec25
David Obwaller <david.obwaller@uibk.ac.at>
2019-06-13 18:42:42 +0200

    Added module `Lac.Analysis.Rules` and `Lac.Analysis.Rules.Match` (stub)

---
+++ src/Lac/Analysis/Rules.hs
@@ -0,0 +1,7 @@
+module Lac.Analysis.Rules (
+    module E
+  ) where
+
+import           Lac.Analysis.Rules.Match as E
+import           Lac.Analysis.Rules.Nil as E
+import           Lac.Analysis.Rules.Node as E
---
+++ src/Lac/Analysis/Rules/Match.hs
@@ -0,0 +1,6 @@
+module Lac.Analysis.Rules.Match where
+
+import           Lac.Analysis.Rules.Common
+
+ruleMatch :: Ctx -> Text -> Typed -> Typed -> Gen ()
+ruleMatch = undefined
