b81fe5d8b27d75e5da782a200d3aac57c8a628a3
David Obwaller <david.obwaller@uibk.ac.at>
2019-06-13 22:06:00 +0200

    Extended (match) rule

--- src/Lac/Analysis/Rules/Match.hs
+++ src/Lac/Analysis/Rules/Match.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Lac.Analysis.Rules.Match where
 
 import           Lac.Analysis.Rules.Common
@@ -5,19 +7,33 @@ import           Lac.Analysis.Rules.Common
 import qualified Data.Text.IO              as T
 
 ruleMatch :: Ctx -> Text -> Typed -> (Text, Text, Text) -> Typed -> Gen Ctx
-ruleMatch ctx x e1 (x1, x2, x3) e2 =
+ruleMatch q x e1 (x1, x2, x3) e2 =
   do
     let u = Bound 1
 
-    (_, p) <- splitCtx u ctx [x]
+    (_, p) <- splitCtx u q [x]
     liftIO $ do
       putStrLn "P"
       T.putStrLn (ppCtx p)
 
-    (_, r) <- splitCtx u ctx [x]
+    (_, r) <- splitCtx u q [x]
     r' <- augmentCtx u r [(x1, tyTree), (x2, tyNat), (x3, tyTree)]
     liftIO $ do
       putStrLn "R"
       T.putStrLn (ppCtx r')
 
-    return ctx
+    -- r(vec{a}, a, a, b) = q(vec{a}, a, b)
+    let raaabs = vecCoeffsRev r' $
+                  \case
+                    (b:a1:a2:as) | a1 == a2, all (== a1) as -> True
+                    _ -> False
+    qaabs <- forM raaabs $ \(VecIdx xs, _) ->
+                                              let a = head xs
+                                                  b = last xs
+                                                  vec = replicate (length xs - 2) a ++ [b]
+                                              in
+                                              coeff q (VecIdx vec)
+
+    forM (zip (map snd raaabs) qaabs) $ \(ri, qi) -> tellConstr [CEq (CAtom ri) (CAtom qi)]
+
+    return q
