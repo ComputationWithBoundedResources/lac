dbc3f5b08233541aacf6017c02d5515b8a1dc8b1
David Obwaller <david.obwaller@uibk.ac.at>
2019-06-13 21:03:56 +0200

    Added basic examples to `demo` executable

--- exe/demo/Main.hs
+++ exe/demo/Main.hs
@@ -1,23 +1,60 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 module Main where
 
-import           Data.Expr.Parser  (expr)
-import           Data.Expr.Typed   (ppTyped)
-import           Data.Term         (T (..))
-import qualified Data.Text.IO      as T
+import           Data.Bound
+import           Data.Expr          (Pattern (..))
+import           Data.Expr.Parser   (expr)
+import           Data.Expr.Typed
+import           Data.List.NonEmpty (NonEmpty (..))
+import           Data.Term          (T (..))
+import qualified Data.Text          as T
+import qualified Data.Text.IO       as T
 import           Data.Type
+import           Lac.Analysis.Rules
+import           Lac.Analysis.Types
 import           Lac.TypeInference
 
 import           Text.Parsec
 
-var = V
-
-env :: Env
-env =
-  [ (var "incr", tyFun tyNat tyNat)
-  ]
+main :: IO ()
+main = go
+  where
+    go = getExpr >>= print >> go
 
 getExpr = (\(Right x) -> x) . parse expr "<stdin>" <$> getLine
 
-main = undefined
+runExample :: Gen Ctx -> IO ()
+runExample f =
+  do
+    (result, Output{..}) <- runGen f
+    case result of
+      Right ctx -> do
+        putStrLn "--"
+        putStrLn "-- FINAL CONTEXT --"
+        putStrLn "--"
+        T.putStrLn $ ppCtx ctx
+        T.putStrLn $ T.intercalate "\n" (map ppConstr outConstraints)
+      Left _ ->
+        putStrLn "error"
+
+-- node
+example1 =
+  let u = Bound 1
+  in
+  runExample $ do
+    ctx <- freshCtx
+    ctx' <- augmentCtx u ctx [("l", tyTree), ("x", tyNat), ("r", tyTree)]
+    liftIO $ T.putStrLn (ppCtx ctx')
+    ruleNode ctx' "l" "r"
+
+-- match
+example2 =
+  let u = Bound 1
+  in
+  runExample $ do
+    ctx <- freshCtx
+    ctx' <- augmentCtx u ctx [("t", tyTree)]
+    liftIO $ T.putStrLn (ppCtx ctx')
+    ruleMatch ctx' "t" (TyVar "e1") ("l", "x", "r") (TyLit (TyLNode (TyVar "l") (TyVar "x") (TyVar "r")))
