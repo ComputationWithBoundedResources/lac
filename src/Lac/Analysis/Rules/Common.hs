6f22db11e434ac80b3e32ab525540ce6948e1c56
David Obwaller <david.obwaller@uibk.ac.at>
2019-06-14 07:08:45 +0200

    Export `Data.List.Ext.equal` from `Lac.Analysis.Rules.Common`

--- src/Lac/Analysis/Rules/Common.hs
+++ src/Lac/Analysis/Rules/Common.hs
@@ -4,6 +4,7 @@ module Lac.Analysis.Rules.Common (
 
 import           Data.Bound         as E
 import           Data.Expr.Typed    as E
+import           Data.List.Ext      as E (equal)
 import           Data.Text          as E (Text)
 import           Data.Type          as E
 import           Lac.Analysis.Types as E
