503de04ecd6a63d1fe1de5a1826947df35a52cf1
David Obwaller <david.obwaller@uibk.ac.at>
2019-06-27 10:40:21 +0200

    Added assertion to (cmp) rule

--- src/Lac/Analysis/Rules/Cmp.hs
+++ src/Lac/Analysis/Rules/Cmp.hs
@@ -3,4 +3,9 @@ module Lac.Analysis.Rules.Cmp where
 import           Lac.Analysis.Rules.Common
 
 ruleCmp :: Ctx -> Text -> Text -> Gen Ctx
-ruleCmp q _ _ = return q
+ruleCmp q x1 x2 =
+  do
+    let u = Bound 1
+    (_, r) <- splitCtx u q [x1, x2]
+    assert (ctxEmpty r) $ "ruleCmp: Q - { " <> x1 <> ", " <> x2 <> " } not empty"
+    return r
