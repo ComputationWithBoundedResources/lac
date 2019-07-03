5ae86aec4eca8875d5ea53026e993a1d38964584
David Obwaller <david.obwaller@uibk.ac.at>
2019-07-03 17:01:38 +0200

    Updated (var) rule

--- src/Lac/Analysis/Rules/Var.hs
+++ src/Lac/Analysis/Rules/Var.hs
@@ -4,8 +4,10 @@ module Lac.Analysis.Rules.Var where
 
 import           Lac.Analysis.Rules.Common
 
-ruleVar :: Ctx -> Text -> Gen Ctx
-ruleVar q _ =
+ruleVar :: Ctx -> Text -> Gen ProofTree
+ruleVar q x =
   do
+    setRuleName "var"
     assert (numVarsCtx q == 1) "ruleVar: context must have length 1"
-    return q
+    -- TODO: check for variable
+    conclude q (TyVar x) q
