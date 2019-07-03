0514ffbb9850edb2ee052043a38d4ffc1bbef516
David Obwaller <david.obwaller@uibk.ac.at>
2019-07-03 14:14:39 +0200

    Updated (ite) rule

--- src/Lac/Analysis/Rules/Ite.hs
+++ src/Lac/Analysis/Rules/Ite.hs
@@ -2,16 +2,21 @@ module Lac.Analysis.Rules.Ite where
 
 import           Lac.Analysis.Rules.Common
 
-ruleIte :: Rule -> Ctx -> Text -> Typed -> Typed -> Gen Ctx
+ruleIte :: Rule -> Ctx -> Text -> Typed -> Typed -> Gen ProofTree
 ruleIte dispatch q x e1 e2 =
   do
     (_, q1) <- splitCtx (Bound 1) q [x]
 
-    qe1 <- dispatch q1 e1
-    qe2 <- dispatch q1 e2
+    t1@(ProofTree (_, _, qe1) _ _ _) <- dispatch q1 e1
+    t2@(ProofTree (_, _, qe2) _ _ _) <- dispatch q1 e2
 
     r <- returnCtx (Bound 1)
     eqReturnCtx qe1 qe2
     eqReturnCtx qe2 r
 
-    return r
+    return $
+      ProofTree
+        (q, TyIte (TyVar x, tyBool) (hole, tyBool) (hole, tyBool), r)
+        (RuleName "ite")
+        []
+        [t1, t2]
