4b066617d018024527247f3815106e2abae31693
David Obwaller <david.obwaller@uibk.ac.at>
2019-07-03 17:03:02 +0200

    Updated examples

--- exe/demo/Main.hs
+++ exe/demo/Main.hs
@@ -4,14 +4,17 @@
 module Main where
 
 import           Data.Bound
-import           Data.Expr.Parser   (expr)
+import           Data.Expr
+import           Data.Expr.Parser       (expr)
 import           Data.Expr.Typed
-import qualified Data.Text          as T
-import qualified Data.Text.IO       as T
+import qualified Data.Text              as T
+import qualified Data.Text.IO           as T
 import           Data.Type
+import           Lac.Analysis.ProofTree
 import           Lac.Analysis.Rules
 import           Lac.Analysis.Types
 
+import           Data.List.NonEmpty     (NonEmpty (..))
 import           Text.Parsec
 
 main :: IO ()
@@ -21,18 +24,13 @@ main = go
 
 getExpr = (\(Right x) -> x) . parse expr "<stdin>" <$> getLine
 
-runExample :: Show a => Gen a -> IO ()
+runExample :: Gen ProofTree -> IO ()
 runExample f =
   do
     (result, Output{..}) <- runGen f
     case result of
-      Right retval -> do
-        putStrLn "--"
-        putStrLn "-- RETURN VALUE --"
-        putStrLn "--"
-        print retval
-        putStrLn "constraints:"
-        T.putStrLn $ T.intercalate "\n" (map ppConstr outConstraints)
+      Right t ->
+        T.writeFile "proof.tex" (latexProofTree t)
       Left e ->
         print e
 
@@ -43,7 +41,6 @@ example1 =
   runExample $ do
     ctx <- freshCtx
     ctx' <- augmentCtx u ctx [("l", tyTree), ("x", tyNat), ("r", tyTree)]
-    liftIO $ T.putStrLn (ppCtx ctx')
     ruleNode ctx' "l" "r"
 
 -- match
@@ -52,19 +49,18 @@ example2 =
   in
   runExample $ do
     ctx <- freshCtx
-    ctx' <- augmentCtx u ctx [("t", tyTree)]
-    liftIO $ T.putStrLn (ppCtx ctx')
-    ruleMatch dispatch ctx' "t" (TyVar "e1") ("l", "x", "r") (TyLit (TyLNode (TyVar "l") (TyVar "x") (TyVar "r")))
+    ctx' <- augmentCtx u ctx [("t", tyTree), ("e1", tyTree)]
+    let e =
+          TyMatch
+            (TyVar "t", tyTree)
+            ((PNil, (TyVar "e1", tyTree))
+             :| [(PNode "l" "x" "r", (TyLit (TyLNode (TyVar "l") (TyVar "x") (TyVar "r")), tyTree))])
+    dispatch ctx' e
 
 example3 =
   let u = Bound 1
   in
   runExample $ do
-    ctx <- freshCtx
-    ctx' <- augmentCtx u ctx [("t1", tyTree), ("t2", tyTree)]
-    i1 <- idx 1 ctx'
-    i2 <- idx 2 ctx'
-    liftIO $ do
-      print i1
-      print i2
-    return ctx'
+    q <- freshCtx
+    q' <- augmentCtx u q [("x", tyTree), ("y", tyTree)]
+    dispatch q' (TyVar "x")
