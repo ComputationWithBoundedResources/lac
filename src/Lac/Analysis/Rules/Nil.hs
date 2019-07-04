55565650c6600ab4a01e654140ae6430e98ad835
David Obwaller <david.obwaller@uibk.ac.at>
2019-07-04 15:55:29 +0200

    Fixed (nil) rule

--- src/Lac/Analysis/Rules/Nil.hs
+++ src/Lac/Analysis/Rules/Nil.hs
@@ -8,6 +8,8 @@ import           Lac.Analysis.Rules.Common
 ruleNil :: Ctx -> Typed -> Gen ProofTree
 ruleNil q e =
   do
+    setRuleName "nil"
+
     assert (ctxEmpty q) $ "ruleNil: context not empty"
 
     q' <- returnCtx (Bound 1)
