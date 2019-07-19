b4bb9a24c6e139cde87de2ae703cdd3fd969d74a
David Obwaller <david.obwaller@uibk.ac.at>
2019-07-19 09:59:58 +0200

    Revised (var) rule
    - preserve potential in returned annotation

--- src/Lac/Analysis/Rules/Var.hs
+++ src/Lac/Analysis/Rules/Var.hs
@@ -8,6 +8,23 @@ ruleVar :: Ctx -> Text -> Gen ProofTree
 ruleVar q x =
   do
     setRuleName "var"
+
     assert (numVarsCtx q == 1) "ruleVar: context must have length 1"
+
     -- TODO: check for variable
-    conclude q (TyVar x) q
+
+    let u = Bound 1
+    ((_, ty), _) <- splitCtx' u q x
+
+    if ty == tyTree
+      then do
+        q' <- returnCtx u
+
+        -- preserve potential
+        q1 <- coeff q (IdIdx x)
+        q'x <- coeff q' AstIdx
+        accumConstr [ CEq (CAtom q1) (CAtom q'x) ]
+
+        conclude q (TyVar x) q'
+      else
+        conclude q (TyVar x) q
