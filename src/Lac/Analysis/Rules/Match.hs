76e4246ea1de40377719370f4da0ae9d6dbb13eb
David Obwaller <david.obwaller@uibk.ac.at>
2019-07-31 14:20:25 +0200

    Generate constraints in (match) rule

--- src/Lac/Analysis/Rules/Match.hs
+++ src/Lac/Analysis/Rules/Match.hs
@@ -3,8 +3,10 @@
 
 module Lac.Analysis.Rules.Match where
 
+import           Control.Monad             (when)
 import           Data.Expr.Types           (Pattern (..))
 import           Data.List.NonEmpty        (NonEmpty (..))
+import qualified Data.Text.IO              as T
 import           Lac.Analysis.Rules.Common
 
 ruleMatch :: Rule -> Ctx -> Text -> Typed -> (Text, Text, Text) -> Typed -> Gen ProofTree
@@ -19,9 +21,17 @@ ruleMatch dispatch q x e1 (x1, x2, x3) e2 =
     (_, r) <- splitCtx' u q x
     r' <- augmentCtx u r [(x1, tyTree), (x2, tyNat), (x3, tyTree)]
 
-    -- TODO: r(vec{a}, a, a, b) = q(vec{a}, a, b)
+    -- r_{(\vec{a}, a, a, b)} = q_{(\vec{a}, a, b)}
+    forVec q (only1 x `after` dropAllBut [x] q) $ \((a1, c1), qab) ->
+      forVec r' (only2 x1 x3 `after` dropAllBut [x1, x3] q) $ \((a2, a3, c2), raab) ->
+        when (and [a1 == a2, a2 == a3, c1 == c2]) $
+          accumConstr [ CEq (CAtom qab) (CAtom raab) ]
 
-    -- TODO: p(vec{a}, c) = sum{a+b=c} q(vec{a}, a, b)
+    -- p_{(\vec{a}, c)} = \sum_{a+b=c} q_{(\vec{a}, a, b)}
+    forVec q (only1 x `after` dropAllBut [x] q) $ \((a, b), qab) ->
+      forVec p (onlyCost `after` dropCtxVars q) $ \(c, pc) ->
+        when (c == a + b) $
+          accumConstr [ CEq (CAtom qab) (CAtom pc) ]
 
     -- r_{m+1} = r_{m+2} = q_{m+1}
     rx1 <- coeff r' (IdIdx x1)
@@ -32,7 +42,15 @@ ruleMatch dispatch q x e1 (x1, x2, x3) e2 =
       , CEq (CAtom rx3) (CAtom qx)
       ]
 
-    -- TODO: r_{(\vec{0}, 1, 0, 0)} = r_{(\vec{0}, 0, 1, 0)} = q_{m+1}
+    -- r_{(\vec{0}, 1, 0, 0)} = r_{(\vec{0}, 0, 1, 0)} = q_{m+1}
+    forVec r' (only2 x1 x3 `after` dropCtxVars q) $ \(a, r100) ->
+      forVec r' (only2 x1 x3 `after` dropCtxVars q) $ \(b, r010) ->
+        case (a, b) of
+          ((1, 0, 0), (0, 1, 0)) ->
+            accumConstr [ CEq (CAtom r100) (CAtom r010)
+                        , CEq (CAtom r010) (CAtom qx)
+                        ]
+          _ -> return ()
 
     -- recursive calls
     q1' <- prove dispatch p  e1
