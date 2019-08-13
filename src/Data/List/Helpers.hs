da5da02e0d01d0af10d956660dcc72743f989c26
David Obwaller <david.obwaller@uibk.ac.at>
2019-08-13 11:39:29 +0200

    Added helpers `split` and `splits`

--- src/Data/List/Helpers.hs
+++ src/Data/List/Helpers.hs
@@ -1,4 +1,9 @@
-module Data.List.Helpers where
+module Data.List.Helpers (
+    delete'
+  , enum
+  , split
+  , splits
+  ) where
 
 delete' :: Eq a => a -> [(a, b)] -> [(a, b)]
 delete' _ [] = []
@@ -14,3 +19,12 @@ enum u = go
     go n = [i : xs | i <- range, xs <- go (n - 1)]
 
     range = [0..u]
+
+split :: Integral a => a -> [(a, a)]
+split x = [(a, x - a) | a <- [0..x]]
+
+splits :: Integral a => Int -> a -> [[a]]
+splits 0 _ = []
+splits 1 x = [[x]]
+splits 2 x = [[a, b] | (a, b) <- split x]
+splits n x = concat $ [map (a:) (splits (n - 1) (x - a)) | a <- [0..x]]
