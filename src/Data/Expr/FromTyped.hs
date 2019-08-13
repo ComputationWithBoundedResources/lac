3df5e5652e08288db88415b4fc8a218845a98889
David Obwaller <david.obwaller@uibk.ac.at>
2019-08-13 14:02:58 +0200

    Added helper `untyped` to `Data.Expr.FromTyped`

--- src/Data/Expr/FromTyped.hs
+++ src/Data/Expr/FromTyped.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Data.Expr.FromTyped where
 
 import           Data.Expr.Typed
 import           Data.Expr.Types
+import           Data.Type
 
 import           Data.List.NonEmpty as NE
 
@@ -23,3 +26,16 @@ fromTyped (TyMatch (e1, _) cs) =
   let cs' = NE.map (\(p, (e, _)) -> (p, fromTyped e)) cs
   in
   Match (fromTyped e1) cs'
+
+untyped :: Expr -> Typed
+untyped =
+  \case
+    -- TODO: Lit
+    Var x -> TyVar x
+    Cmp op e1 e2 -> TyCmp op (rec e1) (rec e2)
+    Ite e1 e2 e3 -> TyIte (rec e1) (rec e2) (rec e3)
+    Let x e1 e2 -> TyLet x (rec e1) (rec e2)
+    App e1 e2 -> TyApp (rec e1) (rec e2)
+    -- TODO: Match, Abs
+  where
+    rec e = (untyped e, tyHole)
