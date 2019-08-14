35dbcaa1bf5ae62f1e0b255571b9dfb70afe9693
David Obwaller <david.obwaller@uibk.ac.at>
2019-08-14 14:14:14 +0200

    Added dependency `data-default`; provide default `Bound` value

--- src/Data/Bound.hs
+++ src/Data/Bound.hs
@@ -2,5 +2,10 @@ module Data.Bound (
     Bound(..)
   ) where
 
+import           Data.Default
+
 newtype Bound = Bound { unBound :: Int }
   deriving (Eq, Show)
+
+instance Default Bound where
+  def = Bound 2
