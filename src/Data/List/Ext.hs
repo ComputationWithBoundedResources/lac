f7feceaa08f84c7289805e3e305f5b142f12f64f
David Obwaller <david.obwaller@uibk.ac.at>
2019-08-14 10:26:09 +0200

    Moved code from `Data.List.Helpers` to `Data.List.Ext`

--- src/Data/List/Ext.hs
+++ src/Data/List/Ext.hs
@@ -1,8 +1,13 @@
 module Data.List.Ext (
     module E
+  , delete'
+  , elemElem
+  , enum
   , equal
   , for
-  , elemElem
+  , split
+  , splits
+  , subseq
   ) where
 
 import           Data.List as E
@@ -23,3 +28,36 @@ elemElem x ys =
   case break (== x) ys of
     (_, [])      -> False
     (_, (_:ys')) -> x `elem` ys'
+
+delete' :: Eq a => a -> [(a, b)] -> [(a, b)]
+delete' _ [] = []
+delete' k ((k1, v1) : xs)
+  | k == k1   = delete' k xs
+  | otherwise = (k1, v1) : delete' k xs
+
+enum :: Int -> Int -> [[Int]]
+enum u = go
+  where
+    go 0 = []
+    go 1 = map (\x -> [x]) range
+    go n = [i : xs | i <- range, xs <- go (n - 1)]
+
+    range = [0..u]
+
+split :: Integral a => a -> [(a, a)]
+split x = [(a, x - a) | a <- [0..x]]
+
+splits :: Integral a => Int -> a -> [[a]]
+splits 0 _ = []
+splits 1 x = [[x]]
+splits 2 x = [[a, b] | (a, b) <- split x]
+splits n x = concat $ [map (a:) (splits (n - 1) (x - a)) | a <- [0..x]]
+
+subseq :: (a -> a -> Bool) -> [a] -> [[a]]
+subseq p = go []
+  where
+    go acc    []     = [reverse acc]
+    go []     (y:ys) = go [y] ys
+    go (a:as) (y:ys)
+        | p a y      =                  go (y:a:as) ys
+        | otherwise  = reverse (a:as) : go [y]      ys
