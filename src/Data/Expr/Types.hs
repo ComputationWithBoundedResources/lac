bc7d3568a82554ae9e5cd95ec59de1c4c86fd2b7
David Obwaller <david.obwaller@uibk.ac.at>
2019-08-22 17:28:32 +0200

    Find unbound and shadowed variables

--- src/Data/Expr/Types.hs
+++ src/Data/Expr/Types.hs
@@ -4,9 +4,9 @@
 
 module Data.Expr.Types where
 
-import           Data.List.NonEmpty (NonEmpty)
+import           Data.List.NonEmpty (NonEmpty ((:|)))
 import qualified Data.List.NonEmpty as NE
-import           Data.Maybe         (mapMaybe)
+import           Data.Maybe.Ext     (mapMaybe, orElse)
 import           Data.Set           (Set)
 import qualified Data.Set           as S
 import           Data.Text          (Text)
@@ -161,3 +161,90 @@ depth (Let _ e1 e2) = 1 + maximum [depth e1, depth e2]
 depth (App e1 e2) = 1 + maximum [depth e1, depth e2]
 depth (Match t cs) = 2 + depth t + maximum (map (\(_, e) -> depth e) . NE.toList $ cs)
 depth (Abs _ e) = 1 + depth e
+
+-- | Find a shadowed variable in expression.
+shadowed :: Expr -> Maybe Text
+shadowed = shadowed' []
+
+shadowed' :: [Text] -> Expr -> Maybe Text
+shadowed' acc e =
+  case e of
+    Let x e1 e2
+      | x `elem` acc -> Just x
+      | otherwise    -> let acc' = x:acc
+                        in
+                        go acc' e1 `orElse` go acc' e2
+    Abs x e
+      | x `elem` acc -> Just x
+      | otherwise    -> let acc' = x:acc
+                        in
+                        go acc' e
+    Match e1 (c1 :| cs) ->
+      let cs' = c1:cs
+      in
+        foldr orElse Nothing $ go acc e1 : map f cs'
+      where
+        f (PNil, e) = go acc e
+        f (PNode x1 x2 x3, e)
+          | x1 `elem` acc = Just x1
+          | x2 `elem` acc = Just x2
+          | x3 `elem` acc = Just x3
+          | otherwise =
+            let acc' = x1:x2:x3:acc
+            in
+            go acc' e
+    Lit (LNode e1 e2 e3) ->
+      go acc e1 `orElse` go acc e2 `orElse` go acc e3
+    Lit _ ->
+      Nothing
+    Var _ ->
+      Nothing
+    Cmp _ e1 e2 ->
+      go acc e1 `orElse` go acc e2
+    Ite e1 e2 e3 ->
+      go acc e1 `orElse` go acc e2 `orElse` go acc e3
+    App e1 e2 ->
+      go acc e1 `orElse` go acc e2
+  where
+    go = shadowed'
+
+-- | Find an unbound variable in expression.
+unbound :: Expr -> Maybe Text
+unbound = unbound' []
+
+unbound' :: [Text] -> Expr -> Maybe Text
+unbound' acc e =
+  case e of
+    Let x e1 e2 ->
+      let acc' = x:acc
+      in
+      go acc' e1 `orElse` go acc' e2
+    Abs x e ->
+      let acc' = x:acc
+      in
+      go acc' e
+    Var x
+      | x `notElem` acc -> Just x
+      | otherwise       -> Nothing
+    Match e1 (c1 :| cs) ->
+      let cs' = c1:cs
+      in
+        foldr orElse Nothing $ go acc e1 : map f cs'
+      where
+        f (PNil, e) = go acc e
+        f (PNode x1 x2 x3, e) =
+          let acc' = x1:x2:x3:acc
+          in
+          go acc' e
+    Lit (LNode e1 e2 e3) ->
+      go acc e1 `orElse` go acc e2 `orElse` go acc e3
+    Lit _ ->
+      Nothing
+    Cmp _ e1 e2 ->
+      go acc e1 `orElse` go acc e2
+    Ite e1 e2 e3 ->
+      go acc e1 `orElse` go acc e2 `orElse` go acc e3
+    App e1 e2 ->
+      go acc e1 `orElse` go acc e2
+  where
+    go = unbound'
