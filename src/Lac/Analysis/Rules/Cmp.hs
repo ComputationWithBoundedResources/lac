a88dfdc199089f97b6f598769c4eb335ee31ab68
David Obwaller <david.obwaller@uibk.ac.at>
2019-09-09 17:23:07 +0200

    Assert correct data types in (cmp) rule

--- src/Lac/Analysis/Rules/Cmp.hs
+++ src/Lac/Analysis/Rules/Cmp.hs
@@ -5,13 +5,21 @@ module Lac.Analysis.Rules.Cmp where
 import           Lac.Analysis.Rules.Common
 
 ruleCmp :: Ctx -> Typed -> Gen ProofTree
-ruleCmp q e@(TyCmp op (TyVar x1, _) (TyVar x2, _)) =
+ruleCmp q e@(TyCmp op (TyVar x1, τ1) (TyVar x2, τ2)) =
   do
     setRuleName "cmp"
 
     let u = def
 
+    forM_ [(x1, τ1), (x2, τ2)] $ \(x, τ) ->
+      assert
+        (τ == tyNat)
+        ("ruleCmp: " <> x <> " must have type Nat")
+
     (_, r) <- splitCtx u q [x1, x2]
-    assert (ctxEmpty r) $ "ruleCmp: Q - { " <> x1 <> ", " <> x2 <> " } not empty"
+
+    assert
+      (ctxEmpty r)
+      ("ruleCmp: Q - { " <> x1 <> ", " <> x2 <> " } not empty")
 
     conclude q e r
