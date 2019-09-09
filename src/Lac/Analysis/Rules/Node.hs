8b29e406b0b1015d84a520ca216625323b54a8b7
David Obwaller <david.obwaller@uibk.ac.at>
2019-09-09 16:27:27 +0200

    Updated (node) rule

--- src/Lac/Analysis/Rules/Node.hs
+++ src/Lac/Analysis/Rules/Node.hs
@@ -5,25 +5,49 @@
 module Lac.Analysis.Rules.Node where
 
 import           Lac.Analysis.Rules.Common
+import qualified Lac.Analysis.Types.Ctx    as Ctx
+
+import qualified Data.Vector               as V
 
 ruleNode :: Ctx -> Typed -> Gen ProofTree
-ruleNode ctx (TyLit (TyLNode (TyVar x1) (TyVar _) (TyVar x2))) =
+ruleNode q (TyLit (TyLNode (TyVar x1) (TyVar _) (TyVar x2))) =
   do
     setRuleName "node"
 
-    ctx' <- returnCtx def
+    assert
+      (Ctx.length q == 2)
+      "ruleNode: context must contain exactly two variables (trees)"
 
-    q1 <- coeff ctx (RankIdx 1)
-    q2 <- coeff ctx (RankIdx 2)
-    qx' <- coeff ctx' astIdx
+    q' <- returnCtx def
 
-    -- TODO: q100 = q010 = qx'
+    -- q_1 = q_2 = q'_\ast
 
-    -- TODO: q(a,a,b), q(a,b)
+    q1 <- coeff q (RankIdx 1)
+    q2 <- coeff q (RankIdx 2)
+    q'x <- coeff q' astIdx
 
     accumConstr $
       [ CEq (CAtom q1) (CAtom q2)
-      , CEq (CAtom q2) (CAtom qx')
+      , CEq (CAtom q2) (CAtom q'x)
       ]
 
-    conclude ctx (TyLit (TyLNode (TyVar x1) hole (TyVar x2))) ctx'
+    -- q_{(1,0,0)} = q_{(0,1,0)} = q'_\ast
+
+    q100 <- coeff q (VecIdx . V.fromList $ [1, 0, 0])
+    q010 <- coeff q (VecIdx . V.fromList $ [0, 1, 0])
+
+    accumConstr $
+      [ CEq (CAtom q100) (CAtom q010)
+      , CEq (CAtom q010) (CAtom q'x)
+      ]
+
+    -- q_{(a,a,b)} = q'_{(a,b)}
+
+    let (Bound u) = def
+
+    forM_ [(a, b) | a <- [0..u], b <- [0..u]] $ \(a, b) -> do
+      qaab <- coeff q (VecIdx . V.fromList $ [a, a, b])
+      q'ab <- coeff q' (VecIdx . V.fromList $ [a, b])
+      accumConstr [ CEq (CAtom qaab) (CAtom q'ab) ]
+
+    conclude q (TyLit (TyLNode (TyVar x1) hole (TyVar x2))) q'
