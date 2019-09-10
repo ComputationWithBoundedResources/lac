cf950a76c1b4edf2b49a25719003c0c1469561ff
David Obwaller <david.obwaller@uibk.ac.at>
2019-09-10 15:11:14 +0200

    Check context in (var) rule

--- src/Lac/Analysis/Rules/Var.hs
+++ src/Lac/Analysis/Rules/Var.hs
@@ -3,21 +3,24 @@
 module Lac.Analysis.Rules.Var where
 
 import           Lac.Analysis.Rules.Common
+import qualified Lac.Analysis.Types.Ctx    as Ctx
+
+import           Debug.Trace
 
 ruleVar :: Ctx -> Typed -> Gen ProofTree
 ruleVar q (TyVar x) =
   do
     setRuleName "var"
 
-    assert (numVarsCtx q == 1) "ruleVar: context must have length 1"
-
-    -- TODO: check for variable
-
     let u = def
     ((_, ty), _) <- splitCtx' u q x
 
     if ty == tyTree
       then do
+        assert
+          (Ctx.length q == 1)
+          "ruleVar: context must consist of exactly one tree"
+
         q' <- returnCtx u
 
         -- preserve potential
@@ -26,5 +29,9 @@ ruleVar q (TyVar x) =
         accumConstr [ CEq (CAtom q1) (CAtom q'x) ]
 
         conclude q (TyVar x) q'
-      else
+      else do
+        assert
+          (Ctx.length q == 0)
+          "ruleVar: context must not contain trees"
+
         conclude q (TyVar x) q
