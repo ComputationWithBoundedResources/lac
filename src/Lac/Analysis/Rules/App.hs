dbcec42ff78c3827d3815cab6414b39ef6c906a9
David Obwaller <david.obwaller@uibk.ac.at>
2019-09-12 17:46:22 +0200

    Check well-formedness of application

--- src/Lac/Analysis/Rules/App.hs
+++ src/Lac/Analysis/Rules/App.hs
@@ -2,17 +2,29 @@
 
 module Lac.Analysis.Rules.App where
 
+import           Data.Expr.FromTyped       (fromTyped)
+import           Data.Expr.Types           (unApp)
 import           Lac.Analysis.Rules.Common
 import qualified Lac.Analysis.Types.Ctx    as Ctx
 
 import           Control.Monad.Ext         (ifM)
+import           Data.List.NonEmpty        (NonEmpty (..))
 import qualified Data.Vector               as V
+
+import           Debug.Trace
 
 ruleApp :: Rule -> Ctx -> Typed -> Gen ProofTree
 ruleApp _ q1 e =
   do
     setRuleName "app"
 
+    case unApp (fromTyped e) of
+      Just (f :| xs) ->
+        go f xs
+      Nothing ->
+        throwError . AssertionFailed $ "ruleApp: ill-formed expression"
+ where
+  go f xs = do
     -- TODO: lookup annotation
 
     let m = Ctx.length q1
